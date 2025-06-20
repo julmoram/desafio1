044afb8006125ac8757f1fac098cf03102af1fe5
Fatima Juliana Zamora Michel <159466066+julmoram@users.noreply.github.com>
2025-06-20 22:32:55 +0000

    recopilación de datos 1

--- desafio1.py
+++ desafio1.py
@@ -1,2 +1,4 @@
-nombre = input('Escribe tú nombre: ')
-print(nombre)
+nombre = input('Ingresa tu nombre: ')
+edad = int(input('Ingresa tu edad: '))
+altura = float(input('Ingresa tu altura en metros: '))
+print(f'Hola, {nombre}, tienes {edad} años y mides {altura} metros')
